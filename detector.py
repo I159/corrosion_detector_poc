2db0a8131de1c05d69f32a18dca23d057733f2fa
Ilya Pekelny <pekelny@gmail.com>
2017-05-15 18:19:53 +0300

    Black and white

--- detector.py
+++ detector.py
@@ -1,21 +1,25 @@
 import os
 import sys
 
+import numpy as np
 from scipy import misc, signal
 from skimage.feature import greycomatrix, greycoprops
 
 
+TH = .23
+
+
 def pixel_patches(file_path, size=32):
-    arr = misc.imread(file_path)
+    arr = misc.imread(file_path, mode='L')
     for i in range(0, arr.shape[0], 8):
         for j in range(0, arr.shape[1], 8):
             patch = arr[i: i+8, j: j+8]
-            yield patch.reshape(patch.shape[0]*patch.shape[1], patch.shape[2])
+            yield patch
 
 
 def glcm(patches):
     for patch in patches:
-        glcm = greycomatrix(patch, [5], [0], 256, symmetric=True, normed=True)
+        glcm = greycomatrix(patch, [5], [0, np.pi/2], 256, normed=True)
         yield greycoprops(glcm, 'energy')
 
 
@@ -27,8 +31,8 @@ def iter_images(path):
         g = glcm(p)
 
         for v in g:
-            if v[0][0] != 0:
-                print("Hooray!")
+            if np.greater(v, TH):
+                print(v)
 
 if __name__ == "__main__":
     iter_images(sys.argv[1])
